40266e9a49a809eea76d3864021faf7b88e271d2
newtepos <supakit.pnm@gmail.com>
2022-04-18 17:02:16 +0700

    modify text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         I was changed on feature branch
+         I was changed on feature branch 2
         </a>
       </header>
     </div>
